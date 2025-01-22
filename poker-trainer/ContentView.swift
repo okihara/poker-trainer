53aef8890c7db5e4ed43800e7b9f80b28b43123e
okihara <okihara@gmail.com>
2025-01-22 11:37:09 +0900

    Initial Commit

---
+++ poker-trainer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  poker-trainer
+//
+//  Created by Masataka Okihara on 2025/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ poker-trainer/poker_trainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  poker_trainerApp.swift
+//  poker-trainer
+//
+//  Created by Masataka Okihara on 2025/01/22.
+//
+
+import SwiftUI
+
+@main
+struct poker_trainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
